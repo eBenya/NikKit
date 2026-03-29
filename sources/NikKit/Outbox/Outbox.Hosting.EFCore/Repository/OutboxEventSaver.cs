422d5237ed2c6346dbb27eb72d3a79d737d358a6
nrabinov <nrabinov@integrotechnologies.ru>
2026-03-29 13:38:58 +0300

    Added Outbox.Hosting.EFCore.csproj

---
+++ sources/NikKit/Outbox/Outbox.Hosting.EFCore/Repository/OutboxEventSaver.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using NikKit.IEnumerableExtensions;
+using NikKit.Outbox.Abstraction;
+
+namespace NikKit.Outbox.Hosting.EFCore.Repository;
+
+public class OutboxEventSaver(DbContext dbContext): IEventSaver
+{
+    public virtual async Task SaveEventsAsync(IEnumerable<IEvent> events, CancellationToken ctk)
+    {
+        var domainEvents = events.AsList();
+        if (domainEvents.Count == 0) return;
+        var now = DateTimeOffset.UtcNow;
+
+        var messages = domainEvents.Select(@event =>
+        {
+            var id = Guid.CreateVersion7();
+            var content = JsonSerializer.SerializeToDocument(@event, @event.GetType());
+            var eventType = @event.GetType().Name;
+            return new OutboxMessage
+            {
+                Id = id,
+                EventType = eventType,
+                Content = content,
+                Created = now.UtcDateTime,
+                PartitionKey = @event.EventName,
+            };
+        });
+
+        await dbContext.Set<OutboxMessage>().AddRangeAsync(messages, ctk);
+    }
+}
---
+++ sources/NikKit/Outbox/Outbox.Hosting.EFCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NikKit.Outbox.Abstraction;
+using NikKit.Outbox.Hosting.EFCore.Repository;
+
+namespace NikKit.Outbox.Hosting.EFCore;
+
+public static class OutboxServiceCollectionExtensions
+{
+    /// <summary>
+    /// Event saver injection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="conf"></param>
+    /// <typeparam name="TDbContext"></typeparam>
+    /// <returns></returns>
+    public static IServiceCollection AddEventSaver<TDbContext>(this IServiceCollection services, IConfiguration conf)
+        where TDbContext : DbContext
+    {
+        services.Configure<OutboxOptions>(opt => conf.GetSection(OutboxOptions.SectionName).Bind(opt));
+
+        services.AddScoped<IEventSaver, OutboxEventSaver>(sp =>
+            new OutboxEventSaver(sp.GetRequiredService<TDbContext>()));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Custom event saver injection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="conf"></param>
+    /// <typeparam name="TEventSaverRepo">Custom saver version</typeparam>
+    /// <returns></returns>
+    public static IServiceCollection AddCustomEventSaver<TEventSaverRepo>(this IServiceCollection services, IConfiguration conf)
+        where TEventSaverRepo : class, IEventSaver
+    {
+        services.Configure<OutboxOptions>(opt => conf.GetSection(OutboxOptions.SectionName).Bind(opt));
+        services.AddScoped<IEventSaver, TEventSaverRepo>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// OutBox worker registration.
+    /// With default repo realization.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <typeparam name="TDbContext"></typeparam>
+    /// <returns></returns>
+    public static IServiceCollection AddOutboxJob<TDbContext>(this IServiceCollection services, IConfiguration configuration)
+        where TDbContext : DbContext
+    {
+        services.AddOutboxJob<OutboxRepository>(configuration);
+        services.AddScoped<IOutboxRepository, OutboxRepository>(sp =>
+            new OutboxRepository(sp.GetRequiredService<TDbContext>()));
+        return services;
+    }
+}
